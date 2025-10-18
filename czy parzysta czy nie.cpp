c21a51e76ac75b64eb544ac1c67848973513e1ad
PlackiDestroyer67 <filipbarczyk.fb@gmail.com>
2025-10-18 16:36:38 +0200

    Create czy parzysta czy nie.cpp

---
+++ czy parzysta czy nie.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main()
+{
+	int liczba, reszta;
+	cout << "podaj liczbe: ";
+	cin >> liczba;
+	reszta = liczba % 2;
+	if (reszta == 0) {
+		cout << "liczba jest parzysta";
+	}
+	else {
+		cout << "liczba jest nieparzysta";
+	}
+	return 0;
+}
