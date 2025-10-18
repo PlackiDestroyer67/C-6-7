c424f8be10d65206443d1de22e0763e4ab02883b
PlackiDestroyer67 <filipbarczyk.fb@gmail.com>
2025-10-18 16:49:06 +0200

    Create 10-50.cpp

---
+++ 10-50.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main()
+{
+	int liczba;
+	cout << "Liczba";
+		cin >> liczba;
+	if (liczba >= 10 && liczba <= 50)
+	{
+	cout << "tak";
+	}
+		
+
+	
+	return 0;
+}
