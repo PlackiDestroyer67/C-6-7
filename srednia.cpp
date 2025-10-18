2b6a9121fa835d9c81d5d0191eda62cc774bd7af
PlackiDestroyer67 <filipbarczyk.fb@gmail.com>
2025-10-18 12:20:23 +0200

    Create srednia.cpp

---
+++ srednia.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+
+using namespace std;
+
+int main()
+{
+	double a, b, c, srednia;
+
+	cout << "podaj a:";
+	cin >> a;
+	cout << "podaj b:";
+	cin >> b;
+	cout << "podaj c:";
+	cin >> c;
+	srednia = (a + b + c);
+	cout << "srednia:" <<srednia /3;
+	
+	return 0;
