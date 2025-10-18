127672b7107aa40551c9e075193ffd7bf7fc9b02
PlackiDestroyer67 <filipbarczyk.fb@gmail.com>
2025-10-18 16:32:02 +0200

    Create ujemna czy dodatnia.cpp

---
+++ ujemna czy dodatnia.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main()
+{
+	int liczba;
+
+	cout << "Podaj dowolna liczbe calkowita" << endl;
+	cin >> liczba;
+	if (liczba > 0)
+		cout << "liczba dodatnia" << endl;
+	else liczba < 0;
+		cout << "liczba ujemna" << endl;
+
+
+	return 0;
+
+}
