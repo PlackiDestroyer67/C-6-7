9701d792d4d0b2024c143ac1a19beed5ff3f75da
PlackiDestroyer67 <filipbarczyk.fb@gmail.com>
2025-10-18 12:34:57 +0200

    Create poletrojkata.cpp

---
+++ poletrojkata.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+
+using namespace std;
+
+int main()
+{
+	double a, h, Pole_trojkata;
+	cout << "podaj a :";
+	cin >> a;
+	cout << "podaj h:";
+	cin >> h;
+	Pole_trojkata = a * h /2 ;
+	cout << "Pole trojkata=" <<Pole_trojkata ;
+
+		return 0;
+}
