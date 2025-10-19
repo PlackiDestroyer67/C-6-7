4a32d698717ce32def30b5bc2d235c70403d9dc3
PlackiDestroyer67 <filipbarczyk.fb@gmail.com>
2025-10-19 12:24:22 +0200

    Create rozne od 0.cpp

---
+++ rozne od 0.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main()
+{
+	int i;
+	do {
+		cout << "Podaj liczbe:" << endl;
+		cin >> i;
+	} while (i != 0); {
+		
+	}
+
+	return 0;
+
+}
