a4645d56a409f45a63171fe1fb0c7d290fef8f0a
PlackiDestroyer67 <filipbarczyk.fb@gmail.com>
2025-10-19 12:18:08 +0200

    Create kwadraty licz od 1 do10.cpp

---
+++ kwadraty licz od 1 do10.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main()
+{
+	int i, kwadrat;
+	for (int i = 1; i <= 10; i++) {
+	
+		kwadrat = (i * i);
+		cout << kwadrat  << endl;
+	}
+			
+
+	return 0;
+
+}
