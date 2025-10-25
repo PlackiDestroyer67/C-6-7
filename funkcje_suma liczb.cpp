cea33be12f8f40fa8a2778406f142bcab8ab71a1
PlackiDestroyer67 <filipbarczyk.fb@gmail.com>
2025-10-25 14:23:56 +0200

    Create funkcje_suma liczb.cpp

---
+++ funkcje_suma liczb.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstring>
+#include <cctype>
+using namespace std;
+
+    void suma(int a, int b) {
+        cout << a + b << endl;
+        
+        
+
+}
+
+int main() {
+
+    int a, b;
+    cout << "Napisz pierwsza liczbe" << endl;
+    cin >> a;
+    cout << "Napisz druga liczbe" << endl;
+    cin >> b;
+     suma(a, b);
+
+    return 0;
+}
