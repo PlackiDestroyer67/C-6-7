b9f66d9e2f2c45a5d4bc036d1c1520b6857412d9
PlackiDestroyer67 <filipbarczyk.fb@gmail.com>
2025-10-25 14:26:33 +0200

    Create funkcje_kwadrat liczby.cpp

---
+++ funkcje_kwadrat liczby.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstring>
+#include <cctype>
+using namespace std;
+
+    void kwadrat(int x) {
+        cout << x * x << endl;
+        
+        
+
+}
+
+int main() {
+
+    int a;
+    cout << "Napisz  liczbe" << endl;
+    cin >> a;
+    
+     kwadrat(a);
+
+    return 0;
+}
