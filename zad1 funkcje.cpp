c11a3303a471e6d070c46eb7bd4400de3da41c17
PlackiDestroyer67 <filipbarczyk.fb@gmail.com>
2025-10-25 15:07:52 +0200

    Create zad1 funkcje.cpp

---
+++ zad1 funkcje.cpp
@@ -0,0 +1,21 @@
+int roznica(int a, int b) {
+        cout << a - b << endl;
+        return a - b;
+}
+
+
+
+
+int main() {
+
+    int a, b;
+    cout << "Napisz pierwsza liczbe" << endl;
+    cin >> a;
+    
+    cout << "napisz druga liczbe " << endl;
+    cin >> b;
+
+    roznica(a, b);
+
+    return 0;
+}
