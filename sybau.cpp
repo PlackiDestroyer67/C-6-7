82929db9d3d9e6edceb92254e43763d9760758d5
PlackiDestroyer67 <filipbarczyk.fb@gmail.com>
2025-10-25 12:17:51 +0200

    Create sybau.cpp

---
+++ sybau.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<cmath>
+#include<cstring>
+#include<cctype>
+#include<Windows.h>
+using namespace std;
+
+int main() {
+   //2
+    char slowo = 'b';
+    cout << "Podaj slowo;" << endl;
+    cin >> slowo;
+    cout<<  toupper(slowo);
+    for (slowo == 49; slowo <= 1000;slowo++) {
+        cout << "sybau                sybau                   sybau              sybau      " ;
+        Sleep(0.00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000003);
+    }
+
+   
+    
+    return 0;
+} 
