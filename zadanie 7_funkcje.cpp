1cc0f2537d3c527d49d9afccfd54bb4bf14524bf
PlackiDestroyer67 <filipbarczyk.fb@gmail.com>
2025-10-25 15:54:30 +0200

    Create zadanie 7_funkcje.cpp

---
+++ zadanie 7_funkcje.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstring>
+#include <cctype>
+using namespace std;
+
+
+float pole_kola(float r, float pi = 3.14) {
+    return 2 * pi * r;
+}
+
+
+
+int main() {
+    float r;
+
+
+    cout << "podaj liczbe:";
+    cin >> r;
+
+    cout << pole_kola(r);
+    return 0;
+}
