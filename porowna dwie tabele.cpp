d0b8e57ea51975e3b050ea4df261ede57898c97f
PlackiDestroyer67 <filipbarczyk.fb@gmail.com>
2025-10-26 12:17:27 +0100

    Update porowna dwie tabele.cpp

--- porowna dwie tabele.cpp
+++ porowna dwie tabele.cpp
@@ -10,28 +10,28 @@
 
       
 
-        int main() {
-            const int N = 5;
-            int tab1[N];
-            int tab2[N];
+   int main() {
+    const int N = 5;
+    int tab1[N];
+    int tab2[N];
 
-            cout << "podaj 5 elementow do pierwszej tabeli: " << endl;
-            for (int i = 0; i < N; i++) {
-                cin >> tab1[N];
+    cout << "podaj 5 elementow do pierwszej tabeli: " << endl;
+        for (int i = 0; i < N; i++) {
+            cin >> tab1[i];
 
-            }
-            cout << "podaj 5 elementow do drogiej tabeli: " << endl;
-            for (int i = 0; i < N; i++) {
-                cin >> tab2[N];
-            }
-
-            for (int i = 0; i < N; i++) {
-                if (tab1[N] != tab2[N]) {
-                    cout << "tabilce sa rozne!" << endl;
-                }
-
-            }
-            cout << "tablice sa takie same";
-
-            return 0;
         }
+        cout << "podaj 5 elementow do drogiej tabeli: " << endl;
+        for (int i = 0; i < N; i++) {
+            cin >> tab2[i];
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (tab1[i] != tab2[i]) {
+                cout << "tabilce sa rozne!" << endl;
+            }
+
+        }
+        cout << "tablice sa takie same";
+
+    return 0;
+}
